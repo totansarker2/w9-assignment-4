8e029d2d7c4b04ffad392f1cf6b5ebb24d4ff66d
Totan <totansarker2@gmail.com>
2024-02-20 03:42:04 +0600

    seat book design done

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// const seats = document.getElementsByClassName('seat');
-
-// for (i of seats){
-//     // console.log(i);
-//     i.addEventListener('click', function() {
-//         clk(i);
-
-//     })
-// }
-
 const allseats = document.getElementById('all-seat');
 const seatIndex = document.getElementById('seat-index');
 const rightSeats = document.getElementById('right-seats');
@@ -24,6 +14,9 @@ while (letter_index < letters.length) {
     const span = document.createElement('span');
     span.classList.add('flex', 'items-center', 'justify-center', 'bg-gray-100', 'h-16', 'rounded-xl', 'border-2', 'border-red-0', 'hover:border-green-1', 'cursor-pointer', 'select-none');
     span.innerHTML = `${letters[letter_index]}${count}`;
+    span.addEventListener('click', function () {
+        clk(span);
+    })
 
     if (count != 0) {
         if (count <= 2) {
@@ -51,34 +44,27 @@ while (letter_index < letters.length) {
     }
 }
 
-// function clk() {
-//     totalPrice += 550;
-//     ticket_counter++;
-//     // seat.classList.toggle('bg-green');
-//     let container = document.createElement('div');
-//     container.classList.add('grid', 'grid-cols-3', 'place-items-center');
-//     let seat_no = document.createElement('span');
-//     let seat_class = document.createElement('span');
-//     let seat_price = document.createElement('span');
+function clk(seat) {
+    totalPrice += 550;
+    ticket_counter++;
 
+    seat.classList.toggle('bg-gray-100')
+    seat.classList.toggle('bg-green-1');
 
+    let container = document.createElement('div');
+    container.classList.add('grid', 'grid-cols-3', 'place-items-center');
+    let seat_no = document.createElement('span');
+    let seat_class = document.createElement('span');
+    let seat_price = document.createElement('span');
 
-//     seat.style.backgroundColor = 'rgba(29, 209, 0, 1)';
-//     seat_no.innerText = seat.innerText;
-//     seat_class.innerText = 'Economy';
-//     seat_price.innerText = '550';
+    seat_no.innerText = seat.innerText;
+    seat_class.innerText = 'Economy';
+    seat_price.innerText = '550';
 
-//     container.appendChild(seat_no);
-//     container.appendChild(seat_class);
-//     container.appendChild(seat_price);
+    container.appendChild(seat_no);
+    container.appendChild(seat_class);
+    container.appendChild(seat_price);
 
-//     document.getElementById('entry').appendChild(container);
-//     document.getElementById('total-price').innerText = totalPrice;
-
-
-
-// }
-// if (ticket_counter <= 4) {
-//     seat.onclick = clk;
-//     console.log(ticket_counter);
-// }
+    document.getElementById('entry').appendChild(container);
+    document.getElementById('total-price').innerText = totalPrice;
+}
